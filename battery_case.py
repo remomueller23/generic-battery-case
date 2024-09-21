e9ded7dccfdcb10101830d4bffc41ccfacbdf8f0
remomueller23 <166062494+remomueller23@users.noreply.github.com>
2024-09-21 07:08:44 +0200

    nix

--- battery_case.py
+++ battery_case.py
@@ -24,8 +24,8 @@ def main():
     # Collecting user input from the sidebar 
     battery_size = st.sidebar.slider("Batteriegroesse (kWh)", 100, 1500, 800)
     c_rate = st.sidebar.slider("C-rate", 0.25, 1.5, 0.25, 0.25)
-    invest_grid = st.sidebar.slider("Invest Netz", 0, 30000, 0, 10000)
-    j_return = st.sidebar.slider("Return pro 100 kW (CHF)", 5000, 20000, 12500)
+    invest_grid = st.sidebar.slider("Invest Netz", 0, 300000, 0, 10000)
+    j_return = st.sidebar.slider("Return pro 100 kW (CHF)", 5000, 30000, 12500)
     cost_per_kWh = st.sidebar.slider("Kosten pro kWh (CHF)", 100, 800, 250, 50)
     pooler_cut = st.sidebar.slider("Pooler Cut (%)", 0, 35, 20)
     rate_of_return = st.sidebar.slider("Rendite (%)", 0.0, 15.0, 7.5, step=0.1) / 100
